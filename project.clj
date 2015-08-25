3b7c6e21401fd2f7ed9289ab3db73516b7184ca8
Corey Beres <corey.beres@gmail.com>
2015-08-24 22:49:16 -0400

    parse entire county list.

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-soup/clojure-soup "0.1.2"]]
+  :plugins [[lein2-eclipse "2.0.0"]]
   :main ^:skip-aot counties.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/counties/core.clj
+++ src/counties/core.clj
@@ -2,9 +2,34 @@
   (:use jsoup.soup)
   (:gen-class))
 
+(defmacro any [& v] `(boolean (or ~@v)))
+(defmacro all [& v] `(boolean (and ~@v)))
+
+(def url "https://en.wikipedia.org/wiki/List_of_United_States_counties_and_county_equivalents")
+
+(def names
+  ($ (get! url :max-body-size 0)
+     "table.wikitable" tr "td:eq(1)"
+     (text)
+     (map #(.replaceAll % "\\[\\d+\\]" ""))
+     (map #(.replaceFirst % ",.+$" ""))
+     (map #(.replaceFirst % " County$" ""))
+     (map #(.replaceFirst % " Parish$" ""))
+     (map #(.replaceFirst % " Borough$" ""))
+     (map #(.replaceFirst % " Census Area$" ""))
+     ))
+
+;  (doseq
+;    [i (remove #(any (.endsWith % " County")
+;                     (.endsWith % " Parish")
+;                     (.endsWith % " Borough")
+;                     (.endsWith % ", City of")
+;                     (.endsWith % " Census Area")) names)]
+;    (println i)))
+
 (defn -main
   "county counter"
   [& args]
-  (println (sort-by val (frequencies
-    ($ (get! "https://en.wikipedia.org/wiki/List_of_United_States_counties_and_county_equivalents")
-       "table.wikitable tr td:eq(1)" (text))))))
+  (doseq
+    [i (sort-by val (frequencies names))]
+    (println i)))
