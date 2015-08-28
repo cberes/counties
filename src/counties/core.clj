16f0e7401f06c812d1cedf3355dc61c5d8405184
Corey Beres <corey.beres@gmail.com>
2015-08-28 00:20:01 -0400

    unique county names.

--- src/counties/core.clj
+++ src/counties/core.clj
@@ -8,12 +8,14 @@
 (def url "https://en.wikipedia.org/wiki/List_of_United_States_counties_and_county_equivalents")
 
 (defn names []
+  "gets all raw county names"
   ($ (get! url :max-body-size 0)
      "table.wikitable tr td:eq(1)"
      (text)
      (map #(.replaceAll % "\\[\\d+\\]" ""))))
 
 (defn read-all-names []
+  "gets all county names with prefixes and suffixes removed"
   (->>
     (names)
     (map #(.replaceFirst % ",.+$" ""))
@@ -23,6 +25,7 @@
     (map #(.replaceFirst % " Census Area$" ""))))
 
 (defn filter-names []
+  "gets county names, and removes valid names, leaving invalid names"
   (->>
     (names)
     (remove #(any (.endsWith % " County")
@@ -31,12 +34,21 @@
                   (.endsWith % ", City of")
                   (.endsWith % " Census Area")))))
 
+(defn csv [m]
+  "concats map keys and values with a comma"
+  (map #(str (key %) "," (val %)) m))
+
 (defn print-all [coll]
+  "prints every element of the collection on a new line"
   (doseq [i coll] (println i)))
 
 (defn -main
   "county counter"
   [& args]
-  (print-all (if (= 0 (count args))
-               (sort-by val (frequencies (read-all-names)))
-               (filter-names))))
+  (print-all (case args
+               ; filter out valid names to find oddities (for testing)
+               (("-f")) (filter-names)
+               ; print counties with unique names
+               (("-u")) (sort (keys (filter #(= 1 (val %)) (frequencies (read-all-names)))))
+               ; print all counties and frequencies as CSV text
+               (csv (sort-by val (frequencies (read-all-names)))))))
